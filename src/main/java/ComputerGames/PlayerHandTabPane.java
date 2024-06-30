c3c160c9ac38685c8458b48357f5d100cdad95c9
plamoy <plamoy411@yahoo.com>
2024-06-30 00:12:46 -0400

    updates from home

--- src/main/java/ComputerGames/PlayerHandTabPane.java
+++ src/main/java/ComputerGames/PlayerHandTabPane.java
@@ -1,47 +1,72 @@
 package ComputerGames;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+
+import java.util.ArrayList;
 
 public class PlayerHandTabPane extends AnchorPane {
 
     // button to shuffle deck
     // button to shuffle discard pile into deck
 
-    public StackPane playerDeck = new StackPane();
+    public ArrayList<PlayingCard> playerDeck = new ArrayList<PlayingCard>();
+    public ArrayList<PlayingCard> playerDiscardPile = new ArrayList<PlayingCard>();
+    public StackPane playerDeckImage = new StackPane();
     private PlayingCard[] equipmentSlots = new PlayingCard[4];
-    public StackPane playerDiscardPile = new StackPane();
+    public StackPane playerDiscardPileImage = new StackPane();
 
     PlayerHandTabPane() {
         HBox mainHBox = new HBox();
-        AnchorPane playerDeckPane = new AnchorPane();
-        VBox playerHandAndEquipmentVBox = new VBox();
-        HBox playerEquipmentHBox = new HBox();
-        HBox playerHandHBox = new HBox();
-        AnchorPane playerDiscardPane = new AnchorPane();
 
-        playerDeck.setPadding(new Insets(10));
+        //create starting deck and shuffle
+        AnchorPane playerDeckPane = new AnchorPane();
+        playerDeckImage.setPadding(new Insets(10));
+
         PlayingCard deckCard = new PlayingCard();
         deckCard.setCardColor("green");
-        playerDeck.getChildren().add(deckCard);
+        playerDeckImage.getChildren().add(deckCard);
+        Button drawButton = new Button("Draw");
+        drawButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                // take top of deck array pull from array and add to player hand
+                // response if no cards in deck or change deck image or add card counter showing number in array
+            }
+        });
 
+        VBox playerHandAndEquipmentVBox = new VBox();
+        HBox playerHandHBox = new HBox();
+        HBox playerEquipmentHBox = new HBox();
+        Button discardHandButton = new Button("Discard Hand");
+        discardHandButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                // remove all children from playerHandHBox and add to discard pile array
+            }
+        });
         for (int i = 0; i < 4; i++) {
             equipmentSlots[i] = new PlayingCard();
         }
-
         playerEquipmentHBox.getChildren().addAll(equipmentSlots);
 
-        playerDiscardPile.setPadding(new Insets(10));
+        AnchorPane playerDiscardPane = new AnchorPane();
+        playerDiscardPileImage.setPadding(new Insets(10));
         PlayingCard discardCard = new PlayingCard();
         discardCard.setCardColor("blue");
-        playerDiscardPile.getChildren().add(discardCard);
+        playerDiscardPileImage.getChildren().add(discardCard);
+        // add shuffle discard pile and add to bottom of deck
 
-        playerDeckPane.getChildren().add(playerDeck);
+        playerDeckPane.getChildren().add(playerDeckImage);
         playerHandAndEquipmentVBox.getChildren().addAll(playerEquipmentHBox, playerHandHBox);
-        playerDiscardPane.getChildren().add(playerDiscardPile);
+        playerDiscardPane.getChildren().add(playerDiscardPileImage);
         mainHBox.getChildren().addAll(playerDeckPane, playerHandAndEquipmentVBox, playerDiscardPane);
         this.getChildren().add(mainHBox);
     }
